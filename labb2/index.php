64f7fc891b13bf8b9c16b4e574c4b1e8f9e3cddb
Evolkasi <32328591+Evolkasi@users.noreply.github.com>
2018-03-21 16:59:34 +0100

    Thingie
    majigg

---
+++ labb2/index.php
@@ -0,0 +1,28 @@
+<?php
+echo "hi";
+//Including functions
+// require ("resources/functions/functions.php");
+//Create function for the Subpages
+/*
+- Start [Huvudsidan]
++- Läsa bitar av inlägg som News
+++- Och sedan kunna gå in på dessa inlägg
++-
+- About
++- Information om företaget
+- Posts (Syns bara för inloggade)
++- Man skall kunna posta inlägg.
++- Redigera inlägg (sina egna)
++- Man kan titta på andras inlägg i helhet.
++- Gilla inlägg
+- Profil
++- Redigera information.
+++- Namn, Bild, Civilstatus osv.
++- Kan också bara direkt titta på sin profil
+*/
+// if (){
+
+// }
+
+// include ("resources/views/layout.php");
+ ?>
---
+++ resources/views/header.php
@@ -0,0 +1,25 @@
+<body>
+
+
+<header>
+  <!--NAVIGATION-->
+  <nav class="navbar navbar-expand-lg">
+    <!--BRAND-->
+    <a href="#" class="navbar-brand">Webbutvekling 2 - bruuu</a>
+    <!--MENU-->
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"data-target="#navbar" aria-expanded="false" aria-label="Toggle Navigation">
+      <span class="navbar-toggle"></span>
+    </button>
+    <div class=="collapse navbar-collapse" id="navbar">
+      <div class="navbar-nav mr-auto">
+        <a class="nav-item nav-link" href="#">Start</a>
+        <a class="nav-item nav-link" href="#">Posts</a>
+        <a class="nav-item nav-link" href="#">About</a>
+      </div>
+    </div>
+  </nav>
+
+</header>
+
+
+</body>
