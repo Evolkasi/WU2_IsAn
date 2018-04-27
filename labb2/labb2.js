4bae2a1d0300e0c5307019c7115192f6f73956cf
Evolkasi <32328591+Evolkasi@users.noreply.github.com>
2018-04-27 12:09:06 +0200

    Added a bunch of stuff
    I kinda looks now

---
+++ labb2/labb2.js
@@ -0,0 +1,18 @@
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {myFunction()};
+
+  // Get the navbar
+  var toolbar = document.getElementById("toolbar");
+
+  // Get the offset position of the navbar
+  var sticky = toolbar.offsetTop;
+
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      toolbar.classList.add("sticky")
+    } else {
+      toolbar.classList.remove("sticky");
+    }
+  }
